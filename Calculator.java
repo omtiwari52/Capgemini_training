47eee91b38d0ee1ee19df0d8a12d5c684ac5db71
omtiwari52 <tiwariom900@gmail.com>
2025-01-07 11:07:07 +0530

    new files

---
+++ Calculator.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Calculator {
+public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+int a = sc.nextInt();
+int b = sc.nextInt();
+int choice = sc.nextInt();
+switch(choice){
+case 1 : System.out.println(a+b);
+break;
+case 2 : System.out.println(a-b);
+break;
+case 3: System.out.println(a*b);
+break;
+case 4 : System.out.println(a/b);
+break;
+case 5 : System.out.println(a%b);
+}
+}
+}
